203fbb133b7cb1df6dacefa35c330501494fb844
Michael Göksu <mikael_goksu@hotmail.com>
2020-10-05 18:43:07 +0000

    "testing further, and all is failing now as expected"

--- jasmine-testing/spec/calcSpec.js
+++ jasmine-testing/spec/calcSpec.js
@@ -7,7 +7,10 @@ describe("Calculator", function() {
             expect(addition(7, 19)).toBe(26);
         });
         it("should return an error if we don't supply two numbers", function(){
-            expect(addition("Hitchhikers", "guide")).toBe("Error!");
+            spyOn(window, "alert");
+            addition("Hitchhikers", "Guide");
+            expect(window.alert).toHaveBeenCalledWith
+                ("Error!");
         });
     });
 });
